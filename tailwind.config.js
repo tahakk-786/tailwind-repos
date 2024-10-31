d35209d64b652562b5bef4f1c26fdda1f22f0946
tahakk-786 <muhammad.taha.1381@gmail.com>
2024-10-31 15:09:08 +0500

    Project Completed

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    css:"./style.css",
+    features:{
+        "class-name-competition":
+        true,
+        "apply-completion": true,
+        "screen-completion": true,
+        "linting": true,
+    },
+    theme: {
+        extend: {
+            colors: {
+              primary: '#309975',
+            },
+        }
+    }
+}
